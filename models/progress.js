8023bc99f9ae7a097c0e6389a94effbac4948596
Teddy <klausnerteddy@gmail.com>
2020-01-12 09:20:05 -0800

    progress controller and model complete

--- models/progress.js
+++ models/progress.js
@@ -1,12 +1,12 @@
 // Load required packages
 var mongoose = require('mongoose');
 
-// Deffine our progress schema
+// Define our progress schema
 var ProgressSchema = new mongoose.Schema({
     user_id: String,
     question_id: String,
     correct: Boolean,
-    num_attempts: Number 
+    attempt_num: {type:Number, default: 0} 
 });
 
 // Export the Mongoose model
